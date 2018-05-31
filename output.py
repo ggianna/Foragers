450d0edba625645da4974ff6b5d85b0ebda9da88
Angelos Kyriakopoulos <angelkyriako@gmail.com>
2018-05-31 19:24:59 +0300

    added hooks for view state at draw map

--- output.py
+++ output.py
@@ -7,6 +7,7 @@ from GameTimeline import GameTimeline
 
 class Output(object):
   def log(self, msg): pass;
+  def logAction(self, msg, x, y): pass;
   def dump(self): pass
   def write(self): pass
   def writeln(self): pass
@@ -24,6 +25,8 @@ class ConsoleOutput(Output):
     self.msg = "";
     self.nextColor = None;
     self.maxMessages = 10;
+
+    self.actions = [];
     self.timeline = GameTimeline();
 
   def log(self, msg):
@@ -42,6 +45,9 @@ class ConsoleOutput(Output):
     if (len(self.lastMessages) > self.maxMessages):
       self.lastMessages = self.lastMessages[-self.maxMessages:];
     
+  def logAction(self, msg, x, y):
+    self.actions.append({"text": msg, "pos": {"x": x, "y": y}})
+
   def dump(self):
     # DEBUG LINES
 #    print "Messages %d"%(len(self.lastMessages));
@@ -64,18 +70,17 @@ class ConsoleOutput(Output):
     
   def color(self, sColor = None):
     self.nextColor = sColor;
-    
 
   def drawMap(self,  mapToDraw,  attackers,  defenders,  trapColor = "red", treasureColor = "yellow", terrainColor = "green"):
       # Clear screen
       Utils.cls();
-      
-      self.timeline.appendStateActionPair({
-        "treasures": [{"id": "SomeEntity", "pos": {"x": 2, "y": 4}}, {"id": "SomeEntity", "pos": {"x": 4, "y": 4}}],
-        "traps": [{"id": "SomeEntity", "pos": {"x": 1, "y": 5}}, {"id": "SomeEntity", "pos": {"x": 2, "y": 5}}],
-        "allies": [{"id": "SomeEntity", "pos": {"x": 3, "y": 4}}, {"id": "SomeEntity", "pos": {"x": 5, "y": 7}}],
-        "enemies": [{"id": "SomeEntity", "pos": {"x": 1, "y": 6}}, {"id": "SomeEntity", "pos": {"x": 3, "y": 6}}]
-      }, {"text": "something happened", "pos": {"x": 2, "y": 4}})
+
+      viewState = {
+        "treasures": [],
+        "traps": [],
+        "allies": [],
+        "enemies": []
+      }
 
       mapInstance = copy.deepcopy(mapToDraw);
       # Init squares
@@ -92,24 +97,31 @@ class ConsoleOutput(Output):
           if (len(squareTreasures)) > 0:
             # Show the first
             mapInstance.squares[iCnt][iCnt2] = colored(str(squareTreasures[0]), treasureColor);
+
+            viewState["treasures"].append({"id": str(squareTreasures[0]), "pos": {"x": iCnt, "y": iCnt2}})
           else:
             if len(squareFoes) > 0:
               # Show the first
               mapInstance.squares[iCnt][iCnt2] = colored(str(squareFoes[0]), trapColor);
+
+              viewState["enemies"].append({"id": str(squareFoes[0]), "pos": {"x": iCnt, "y": iCnt2}})
             else:
               # If trap in square
               if (len(squareTraps)) > 0:
                 # Show the first
                 mapInstance.squares[iCnt][iCnt2] = colored(str(squareTraps[0]), trapColor);
-          
+
+                viewState["traps"].append({"id": str(squareTraps[0]), "pos": {"x": iCnt, "y": iCnt2}})
+
       # Render soldiers
       for cItem in  attackers:
           mapInstance.squares[cItem.x][cItem.y] = colored(str(cItem),  cItem.color);
+          viewState["allies"].append({"id": str(cItem), "pos": {"x": cItem.x, "y": cItem.y}})
           
       for cItem in  defenders:
           mapInstance.squares[cItem.x][cItem.y] = colored(str(cItem),  cItem.color);
+          viewState["enemies"].append({"id": str(cItem), "pos": {"x": cItem.x, "y": cItem.y}})
                 
-      
       # Show map
       for curRow in range(mapInstance.ySize):
           for curCol in range(mapInstance.xSize):
@@ -118,6 +130,15 @@ class ConsoleOutput(Output):
 
       # Get rid of deep map copy
       del mapInstance;
+
+      # Append state - action pair for the View engine
+      for action in self.actions:
+        self.timeline.appendStateActionPair(viewState, action)
+
+      self.timeline.appendStateActionPair(viewState, {})
+
+      self.actions[:] = []
+
 
   def saveToFile(self, sFilename):
     fOut = open(sFilename, 'w')
