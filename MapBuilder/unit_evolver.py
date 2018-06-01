9a1dc8abac389ec6dc5586dc345f12b20ede4922
Mario Garcia Valdez <mariosky@gmail.com>
2018-06-01 16:31:00 +0300

    evolvedUnits

--- MapBuilder/unit_evolver.py
+++ MapBuilder/unit_evolver.py
@@ -37,6 +37,7 @@ def EvaluateUnit(chromosome=[0.00, 0.10, 0.10, 0.00], currentUnit = None ,curren
         output = Output()
         g = Game(economy, changeableMap, fodderArmy, output, 0.0);
 
+
         score = g.run()
         return  score,
 
@@ -55,7 +56,7 @@ toolbox.register("individual", tools.initRepeat, creator.Individual,
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("evaluate", EvaluateUnit)
 toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=0.1, indpb=0.05)
+toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=0.2, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=4)
 
 
@@ -65,13 +66,13 @@ def main(popsize, generations, currentUnit = None ,currenArmy = None, gameMap = 
     # create an initial population of 300 individuals (where
     # each individual is a list of integers)
     pop = toolbox.population(n=popsize)
-    print pop
+    #print pop
 
     # CXPB  is the probability with which two individuals
     #       are crossed
     #
     # MUTPB is the probability for mutating an individual
-    CXPB, MUTPB = 0.5, 0.2
+    CXPB, MUTPB = 0.6, 0.5
 
     print("Start of evolution")
 
@@ -82,7 +83,7 @@ def main(popsize, generations, currentUnit = None ,currenArmy = None, gameMap = 
     for ind, fit in zip(pop, fitnesses):
         ind.fitness.values = fit
 
-    print("  Evaluated %i individuals" % len(pop))
+    #print("  Evaluated %i individuals" % len(pop))
 
     # Extracting all the fitnesses of
     fits = [ind.fitness.values[0] for ind in pop]
@@ -94,7 +95,7 @@ def main(popsize, generations, currentUnit = None ,currenArmy = None, gameMap = 
     while g < generations:
         # A new generation
         g = g + 1
-        print("-- Generation %i --" % g)
+        # print("-- Generation %i --" % g)
 
         # Select the next generation individuals
         offspring = toolbox.select(pop, len(pop))
@@ -126,7 +127,7 @@ def main(popsize, generations, currentUnit = None ,currenArmy = None, gameMap = 
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
 
-        print("  Evaluated %i individuals" % len(invalid_ind))
+        #print("  Evaluated %i individuals" % len(invalid_ind))
 
         # The population is entirely replaced by the offspring
         pop[:] = offspring
@@ -139,15 +140,16 @@ def main(popsize, generations, currentUnit = None ,currenArmy = None, gameMap = 
         sum2 = sum(x * x for x in fits)
         std = abs(sum2 / length - mean ** 2) ** 0.5
 
-        print("  Min %s" % min(fits))
-        print("  Max %s" % max(fits))
-        print("  Avg %s" % mean)
-        print("  Std %s" % std)
+        #print("  Min %s" % min(fits))
+        #print("  Max %s" % max(fits))
+        #print("  Avg %s" % mean)
+        #print("  Std %s" % std)
 
     print("-- End of (successful) evolution --")
 
     best_ind = tools.selBest(pop, 1)[0]
     print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+    return best_ind,best_ind.fitness.values[0]
 
 
 if __name__ == "__main__":
@@ -158,18 +160,23 @@ if __name__ == "__main__":
 
     army = Game.selectArmy(economy, curmap, armyColor="white", output=Output(),
                     aUnitPool=['SoldierClass', 'TechnicianClass', 'MageClass'])
-    for unit in  army:
+    scores = []
+    for index, unit in enumerate(army):
+        print "Treasures:",len(curmap.treasures),
 
-        index = 0
+        evolvedParams, best = main(popsize=100, generations=20,  currentUnit = army[index] ,currenArmy = army, gameMap = curmap, economy = economy, unitIdx = index)
+        scores.append(( best, evolvedParams))
+        army[index].strategy.curiosity = evolvedParams[0]
+        army[index].strategy.groupSpirit = evolvedParams[1]
+        army[index].strategy.riskiness = {"SoldierClass": evolvedParams[2], "TrapClass": evolvedParams[3],
+                                          "TowerClass": evolvedParams[4]};  # Base probability to get closer to interact
+        army[index].strategy.fasting = evolvedParams[5];  # Base probability to return if hungry
+        army[index].strategy.greed = evolvedParams[6]
+        for u in army[:5]:
+            print u.strategy.curiosity
 
-    main(popsize=10, generations=20,  currentUnit = army[index] ,currenArmy = army, gameMap = curmap, economy = economy, unitIdx = index)
-
-    # Super Difficult level:
-    # printMap([0.8031057995895188, 0.20136711521936546, 0.8194173185392594, 0.6529689056798653])
-
-    # Difficult level
-    # EvaluateMap([0.03002840378487342, 0.01868680484970526, 0.12188740611196378, 0.08543245449223595])
-    # printMap([0.03002840378487342, 0.01868680484970526, 0.12188740611196378, 0.08543245449223595])
+    scores.sort(reverse=True)
+    for score in scores[:5]:
+        print(score)
 
 
-    # Medium Level
